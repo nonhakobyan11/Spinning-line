80e2090f4b3529b94a464e56da272886f207db11
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-10 22:41:53 +0400

    Task 3

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -3,7 +3,7 @@ package com.homework;
 public class Main {
 
     public static void main(String[] args)  {
-        String str = "|/-\\|/-\\";
+        String str = "|/-\\";
         String output = "";
         char l;
         for (; ;) {
